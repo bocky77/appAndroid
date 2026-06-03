52bebe78214fc64fbb715629dcdc1eea9b8aecc9
Andres Angucho <julian.angucho@estudiante.fup.edu.co>
2026-06-03 11:53:38 -0500

    se hace un pequeño ajuste del inicio

--- INICIOKTY/app/src/main/java/com/example/iniciokty/MainActivity.java
+++ INICIOKTY/app/src/main/java/com/example/iniciokty/MainActivity.java
@@ -1,9 +1,6 @@
-package com.example.appandroid;
+package com.example.iniciokty;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,16 +19,6 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button btnIngreso = findViewById(R.id.btnIngresar);
-
-        btnIngreso.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentLogin = new Intent(MainActivity.this, menu.class);
-                startActivity(intentLogin);
-            }
         });
     }
 }
